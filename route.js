7773d73ec8de54f9157c6abe1c8e36495dd5e6dc
J23023 <san-j23023@sangi.jp>
2025-05-26 12:20:56 +0900

    Add route* and move work* to mensetsu/

---
+++ route.js
@@ -0,0 +1,22 @@
+(function() {
+	"use strict";
+
+	const { createApp, ref } = Vue;
+
+	createApp({
+		setup() {
+			const tabs = ref([
+				{ id: "spi", url: "./spi", name: "SPI" },
+				{ id: "mensetsu", url: "./mensetsu/work-manage-account.html", name: "面接" },
+			]);
+			const currentTab = ref(tabs.value[0]["id"]);
+			const switchTab = (tab) => {
+				currentTab.value = tab;
+			};
+
+			return window.d = {
+				tabs, currentTab, switchTab,
+			};
+		},
+	}).mount("#app");
+})();
