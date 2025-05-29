a89aed6d175992afdc7f4c9609ee78f528c91423
ei70 <san-j23005@sangi.jp>
2025-05-29 10:39:18 +0900

    chore(store): setup Pinia in the project

--- v2/src/main.js
+++ v2/src/main.js
@@ -3,10 +3,17 @@ import './assets/font-awesome.css';
 // import './assets/main.css'
 
 import { createApp } from 'vue';
+import { createPinia } from 'pinia'
 import App from './App.vue';
 import router from './router';
 
 import ToastPlugin from 'vue-toast-notification';
 import 'vue-toast-notification/dist/theme-bootstrap.css';
 
-createApp(App).use(router).use(ToastPlugin).mount('#app');
+const pinia = createPinia()
+const app = createApp(App)
+
+app.use(pinia)
+app.use(router)
+app.use(ToastPlugin)
+app.mount('#app')
