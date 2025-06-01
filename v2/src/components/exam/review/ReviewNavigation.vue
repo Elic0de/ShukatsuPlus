c498293a07bb9980f7239e8a568b2d6f6e8378cc
ei70 <san-j23005@sangi.jp>
2025-06-01 15:05:45 +0900

    style: update padding classes in ReviewNavigation.vue (px-4 pt-4)

--- v2/src/components/exam/review/ReviewNavigation.vue
+++ v2/src/components/exam/review/ReviewNavigation.vue
@@ -1,6 +1,6 @@
 <template>
   <nav class="fixed bottom-0 z-50 w-full border-t-2 border-neutral-200 bg-white">
-    <div v-if="question.parts.length > 1" class="flex gap-2 p-4">
+    <div v-if="question.parts.length > 1" class="flex gap-2 px-4 pt-4">
       <button
         v-for="(part, idx) in question.parts"
         :key="part.id"
@@ -10,7 +10,7 @@
         問{{ idx + 1 }}
       </button>
     </div>
-    <div class="px-4 py-4 flex gap-2">
+    <div class="px-4 py-4 flex">
       <button @click="$emit('prev')" class="btn-next">
         <span>前へ</span>
       </button>
