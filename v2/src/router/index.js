84d98bf0ccffd7423396c2ec2a7ee4edfd269897
J23023 <san-j23023@sangi.jp>
2025-06-01 20:37:55 +0900

    v2: Use hash history

--- v2/src/router/index.js
+++ v2/src/router/index.js
@@ -1,4 +1,4 @@
-import { createWebHistory, createRouter } from "vue-router";
+import { createWebHashHistory, createRouter } from "vue-router";
 
 import Welcome from "@/views/Welcome.vue";
 //
@@ -65,7 +65,7 @@ const routes = [
 ];
 
 const router = createRouter({
-	history: createWebHistory(),
+	history: createWebHashHistory(),
 	routes,
 });
 
