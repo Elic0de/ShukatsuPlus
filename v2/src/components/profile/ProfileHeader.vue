48fb6e30e487e5ef56567469b86d5b32ea9593d3
J23023 <san-j23023@sangi.jp>
2025-06-01 22:48:53 +0900

    v2: Add alt text to img in ProfileHeader

--- v2/src/components/profile/ProfileHeader.vue
+++ v2/src/components/profile/ProfileHeader.vue
@@ -2,16 +2,12 @@
 	<div
 		class="flex justify-between border-b-2 border-neutral-200 p-4 text-xl font-bold text-neutral-400"
 	>
-		<img
-			class="_2Flrl"
-			src="https://d35aaqx5ub95lt.cloudfront.net/images/103c3f969d4037b39a2771330f5a3bd8.svg"
-			style="visibility: hidden"
-		/>
 		{{ title }}
 		<a :href="settingsLink">
 			<img
 				class="_2Flrl"
 				src="https://d35aaqx5ub95lt.cloudfront.net/images/103c3f969d4037b39a2771330f5a3bd8.svg"
+				alt="設定"
 			/>
 		</a>
 	</div>
