f5787f660ec970804824dcc0ec3a1b50cc4c3bcb
ei70 <san-j23005@sangi.jp>
2025-06-02 20:40:40 +0900

    commit

--- v2/src/views/auth/LineAuth.vue
+++ v2/src/views/auth/LineAuth.vue
@@ -4,17 +4,21 @@
     import { watch } from "vue";
 	import { useLiff } from "@/composables/useLiff";
 	import { useSessionStore } from "@/stores/session";
-
+    import { useUserStore } from "@/stores/user";
+    import { api } from "@/apis/apiFactory";
+    
 	const { login, getIdToken, isReady } = useLiff();
 	const store = useSessionStore();
+    const userStore = useUserStore();
 
     watch(isReady, async (ready) => {
         if (ready) {
             login();
             const idToken = getIdToken();
             if (idToken) {
-                const response = await postIdTokenToGAS(idToken);
+                const response = await api.postIdTokenToGAS(idToken);
                 store.login(response.sessionId, response.userId);
+                userStore.user.value.username = response.userName;
             }
         }
     });
