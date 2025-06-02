0b4893fc14c3d635e08478087db0321f26d350d2
ei70 <san-j23005@sangi.jp>
2025-06-02 20:01:41 +0900

    commit

--- v2/src/router/index.js
+++ v2/src/router/index.js
@@ -48,7 +48,7 @@ const routes = [
 	{ path: "/auth", component: AuthPage },
 	{ path: "/login", component: AccountManager },
 	{ path: "/signup", component: SignUp },
-	{ path: "/oauth/line", component: LineAuth },
+	{ path: "/oauth/line", component: LineAuth, props: true },
 ];
 
 const router = createRouter({
